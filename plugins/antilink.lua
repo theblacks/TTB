ecb1c5914a49da43f7f807ddaf1a4b6ed0b5935e
theblacks <amirhack555@gmail.com>
2016-03-09 12:17:40 -0800

    Create antilink.lua

---
+++ plugins/antilink.lua
@@ -0,0 +1,19 @@
+local function run(msg)
+    if msg.to.type == 'chat' and not is_momod(msg) then
+        chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+        return 'فرستادن لینک ممنوع است'
+    end
+end
+
+return {
+    patterns = {
+   "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+   "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
+   "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+   "[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+   "[Hh][Tt][Tt][Pp]://",
+   "[Ww][Ww][Ww]:",
+
+    }, 
+run = run
+}
