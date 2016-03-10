f1c9a5a596c635cb3a2c91ca51519c41f5f7486c
theblacks <amirhack555@gmail.com>
2016-03-10 09:48:54 -0800

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,29 @@
+do
+
+function run(msg, matches)
+  return 'Tesla Smart Bot v1.5'.. [[ 
+  
+  creators:
+  @badrabbit
+  @mr_hopes
+  Antispam Bot: @Teslasmart
+  Channel: @TeslaPe .. @TeslaEn
+  Sudo (Admin): @badrabbit .. @mr_hopes
+  
+  Special thanks to:
+  @c_techteam .. @c_technology
+  @godhackers
+  @darkboyhkr
+  http://s6.uplod.ir/i/00738/u2ten2i05plf.jpg]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^version$"
+  }, 
+  run = run 
+}
+
+end
