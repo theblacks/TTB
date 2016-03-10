cfd2ec1208aacd607c1b0d25dd649152597b5f2e
theblacks <amirhack555@gmail.com>
2016-03-10 09:35:10 -0800

    Update addadmin.lua

--- plugins/addadmin.lua
+++ plugins/addadmin.lua
@@ -1,15 +1,34 @@
 do
-local function run(msg, matches)
-local sudo = 179526469
-    if matches[1]:lower() == "add sudo" then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..sudo, ok_cb, false)
-    end
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
 end
- 
+
+local function run(msg, matches)
+  local user = 179526469
+
+  if matches[1] == "support" then
+    user = 'user#id'..user
+  end
+
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "سازنده ربات وارد گروه شد"
+  else 
+    return 'This isnt a chat group!'
+  end
+
+end
+
 return {
+  description = "support", 
   patterns = {
-    "^addsudo$",
-  },
-  run = run
+    "^(support)$"
+  }, 
+  run = run 
 }
+
 end
